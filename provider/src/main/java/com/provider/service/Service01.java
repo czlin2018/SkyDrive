17bd6a4ab31d2ff38e8801ecab750c8c416013b7
czlin <zelin.chen@aqara.com>
2019-02-27 18:14:25 +0800

    first commit

---
+++ provider/src/main/java/com/provider/service/Service01.java
@@ -0,0 +1,17 @@
+package com.provider.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @描述:
+ * @公司: lumi
+ * @author: 泽林
+ * @创建日期: 2019-02-19-2-27
+ * @创建时间: 下午4:06
+ */
+@Service
+public class Service01{
+    public String hello  ( ){
+        return "hello";
+    }
+}
